3074fdb6b44a78501d09e65be745eb21b6909159
mikael jefferson da silva <mikaeljeffeson@gmail.com>
2022-09-08 09:57:15 -0300

    sla

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 window.onload = function(){
     const bola = document.getElementsByClassName('ball');
+    const btn = document.querySelector('#reset-game');
+    const rgb = document.querySelector('#rgb-color');
+    const  paragrafo = document.querySelector('#answer');
+
     function randoColor(){
         const red = Math.floor(Math.random() * 256);
         const blue = Math.floor(Math.random() * 256);
@@ -15,4 +19,34 @@ window.onload = function(){
         }
         
         backgroundBall();
+        
+        function randomElementClassBall() {
+            const element = bola[Math.floor(Math.random() * bola.length)]
+            const elementColor = [];
+            elementColor.push(element.style.backgroundColor);
+          
+            return elementColor;
+}
+            rgb.innerText = randomElementClassBall();
+            paragrafo.innerText = 'Escolha uma cor';
+
+        function ifColorEqual(event) {
+            if (event.target.style.backgroundColor == rgb.innerText) {
+                paragrafo.innerText = 'Acertou!';
+            } else {
+                paragrafo.innerText = 'Errou! Tente novamente!';
+            }
+        }
+
+        for(let index = 0; index < bola.length;index+=1){
+            bola[index].addEventListener('click', ifColorEqual);
+        }
+        
+        function resetGame() {
+            pAnswer.innerText = 'Escolha uma cor';
+            backgroundBall();
+            rgb.innerText = randomElementClassBall();
+          }
+          
+          buttonResetGame.addEventListener('click', resetGame);
 }
